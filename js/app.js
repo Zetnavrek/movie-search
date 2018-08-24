0bc2e04de24cac258fc82ed08407bc31239aea60
Zetnavrek <vykovi@gmail.com>
2018-08-23 23:08:44 -0500

    Printing and getting selected movie

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 const key= "685d851e2a649cd92b21f6de3a19b278";
+const printingMovies = (movies)=> {
+    movies.forEach((movie) => {
+        let picture = movie.poster_path;
+        let image = `https://image.tmdb.org/t/p/w300${picture}`;
+        let name = movie.title;
+        let id = movie.id;
+        console.log(picture);
+        console.log(image);
+        console.log(name);
+        console.log(id);
+    })
+}
+
 
 fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}`)
 .then((response) => {
@@ -6,6 +19,26 @@ fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}`)
     return response.json();
 }).then((data) => {
     console.log(data);
+    printingMovies(data.results);
+    showMovies(data.results);
 }).catch((err) => {
     console.log('Ups! Ocurrio un error');
 });
+
+
+const showMovies = (data) =>{
+    data.forEach((movie) =>{
+        let id = movie.id;
+        console.log(id);
+fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${key}`)
+.then((response) => {
+    console.log(response);
+    return response.json();
+}).then((data) => {
+    console.log(data);
+    
+}).catch((err) => {
+    console.log('Ups! Ocurrio un error');
+});
+})
+}
